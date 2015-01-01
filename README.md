86964a0b01374ec708533c3c66ebf87d462cbd1c
Nathan Smith <nathanrandal@gmail.com>
2015-01-01 17:11:51 -0500

    working on sections

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Hack Michiana (Future) Website
     git clone -b build https://github.com/HackMichiana/HackMichiana.github.io.git
     pip install -r requirements.txt
     npm install
+    npm install -g bower
+    bower install
     ./build.py
 
 Do stuff...
--- build.py
+++ build.py
@@ -74,8 +74,8 @@ def build():
                         raise
 
     # Build LESS
-    # print 'Compiling LESS to CSS'
-    # call(['node', 'node_modules/.bin/lessc', 'static/less/style.less', 'static/css/style.css'])
+    print 'Compiling LESS to CSS'
+    call(['node', 'node_modules/.bin/lessc', 'static/less/style.less', 'static/css/style.css'])
 
     # copy static files
     print 'Copying static files'
